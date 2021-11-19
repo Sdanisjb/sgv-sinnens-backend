af7d489f7bdff6fbb84d17ee05e999b6217bcd53
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2021-11-19 18:09:32 -0500

    feat (Auth): Autenticación agregada a todos los endpoints necesarios para ejecutar los casos de uso al 40% ya antes especificados

---
+++ app/Http/Controllers/AuthControlador.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthControlador extends Controller
+{
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'datos de login invalidos'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+    }
+}
