b2acdcd930ee4f473138f15ccde30080ba9ae88d
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2021-11-19 18:41:19 -0500

    feat (databaseseeder): Seeder de Usuarios, Roles, Documentos y Vehiculos terminado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin_Logistica;
+use App\Models\Gerente_General;
+use App\Models\Gerente_Tecnico;
+use App\Models\Operador;
+use App\Models\PermisoAutrisa;
+use App\Models\PermisoMTC;
+use App\Models\PermisoTranspMercancia;
+use App\Models\Soat;
+use App\Models\User;
+use App\Models\Vehiculo;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +23,94 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $gerente_general = User::factory()->create([
+            'email' => 'gerentegeneral@sinnens.com'
+        ]);
+
+        Gerente_General::factory()
+            ->count(1)
+            ->for($gerente_general, 'usuario')
+            ->create();
+
+        $gerente_tecnico = User::factory()->create([
+            'email' => 'gerentetecnico@sinnens.com'
+        ]);
+
+        Gerente_Tecnico::factory()
+            ->count(1)
+            ->for($gerente_tecnico, 'usuario')
+            ->create();
+
+        $admin_logistica = User::factory()->create([
+            'email' => 'adminlogistica@sinnens.com'
+        ]);
+
+        Admin_Logistica::factory()
+            ->count(1)
+            ->for($admin_logistica, 'usuario')
+            ->create();
+
+        $operador = User::factory()->create([
+            'email' => 'operador@sinnens.com'
+        ]);
+
+        Operador::factory()
+            ->count(1)
+            ->for($operador, 'usuario')
+            ->create();
+
+        User::factory()->count(5)->create();
+
+
+        /*Seedear Vehiculos */
+        $vehiculos = Vehiculo::factory()->count(3)->create();
+
+        foreach ($vehiculos as $vehiculo) {
+            PermisoAutrisa::factory()
+                ->for($vehiculo, 'vehiculo')
+                ->create();
+            PermisoMTC::factory()
+                ->for($vehiculo, 'vehiculo')
+                ->create();
+            PermisoTranspMercancia::factory()
+                ->for($vehiculo, 'vehiculo')
+                ->create();
+            Soat::factory()
+                ->for($vehiculo)
+                ->create();
+        }
+
+        $vehiculos = Vehiculo::factory()->count(2)->create();
+
+        foreach ($vehiculos as $vehiculo) {
+            PermisoMTC::factory()
+                ->for($vehiculo, 'vehiculo')
+                ->create();
+            PermisoTranspMercancia::factory()
+                ->for($vehiculo, 'vehiculo')
+                ->create();
+            Soat::factory()
+                ->for($vehiculo)
+                ->create();
+        }
+
+        $vehiculos = Vehiculo::factory()->count(2)->create();
+
+        foreach ($vehiculos as $vehiculo) {
+            PermisoTranspMercancia::factory()
+                ->for($vehiculo, 'vehiculo')
+                ->create();
+            Soat::factory()
+                ->for($vehiculo)
+                ->create();
+        }
+
+        $vehiculos = Vehiculo::factory()->count(2)->create();
+
+        foreach ($vehiculos as $vehiculo) {
+            Soat::factory()
+                ->for($vehiculo)
+                ->create();
+        }
     }
 }
