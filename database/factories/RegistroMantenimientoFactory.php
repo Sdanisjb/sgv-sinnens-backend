62cb7892f44b171d1b7f065cbd6ea1de5cb67937
Sandro Caceres <sandro.caceres@ucsp.edu.pe>
2021-11-30 19:16:28 -0500

    bug (RegistroMantenimientoFactory): Arreglado para que no supere el límite de nombre de la empresa

--- database/factories/RegistroMantenimientoFactory.php
+++ database/factories/RegistroMantenimientoFactory.php
@@ -17,7 +17,7 @@ class RegistroMantenimientoFactory extends Factory
             'placa_vehiculo' => $this->faker->bothify('???-###'),
             'fecha_emision' => $this->faker->dateTimeBetween('-3 weeks', 'now'),
             'fecha_salida' => $this->faker->dateTimeBetween('now', '+1 month'),
-            'nombre_taller' => $this->faker->company(),
+            'nombre_taller' => $this->faker->realText(20),
             'km_actual' => $this->faker->optional()->randomNumber(),
             'nro_factura' => $this->faker->numerify('##########'),
             'nro_proforma' => $this->faker->optional()->numerify('##########')
